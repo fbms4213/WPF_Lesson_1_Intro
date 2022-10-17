c1747cc826525a74d2371606a9872718dce0a356
t.novruzov <t.novruzov@STHQ0124-01>
2022-10-17 11:49:03 +0400

    Add project files.

---
+++ Source/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Source;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Title = "Dynamic Title";
+
+
+
+        //Brushes.Bisque
+        Button button = new Button
+        {
+            Content = "Dynamic button",
+            Background = new SolidColorBrush(Colors.BlueViolet),
+            FontSize = 22,
+        };
+
+        Grid.SetColumn(button, 2);
+        Grid.SetRow(button, 1);
+
+        container.Children.Add(button);
+
+    }
+}
